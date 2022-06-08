1be713847859a965ee0c6e92a77f5d4c32310229
TarunKumarP <tarun.kumar@thinkjs.co>
2022-06-08 12:25:09 +0530

    inital commit

---
+++ assignment/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import Search from './components/page1/Search';
+
+function App() {
+  return (
+    <div className='App'>
+      <h3 style={{color:"red"}}>Employee Selection App</h3>
+      <Search />
+    </div>
+  );
+}
+
+export default App;
---
+++ assignment/src/components/page1/Search.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Search = () => {
+  return (
+    <div>
+        <h4>Search</h4>
+        <input type="search" placeholder='Search Employee ' name="firstName" />
+      
+    </div>
+  )
+}
+
+export default Search
+
